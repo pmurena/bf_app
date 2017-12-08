41f6459cad4ba30ad302c9ae7f15e3f588dd1cb2
Marc-Andrea Tarnutzer <mail@marctarnutzer.com>
2017-12-08 11:44:03 +0100

    Added login view

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -4,5 +4,12 @@ var page;
 
 exports.loaded = function(args){
     page = args.object;
+};
+
+exports.signIn = function() {
+    frameModule.topmost().navigate("views/activities/activities");
+};
+
+exports.register = function() {
     frameModule.topmost().navigate("views/activities/activities");
 };
