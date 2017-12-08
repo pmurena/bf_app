9db8c0bdeac8258fb4d69cbada972f6c85e6e2a4
Marc-Andrea Tarnutzer <mail@marctarnutzer.com>
2017-12-08 16:31:06 +0100

    Added view model functionality for login and signup

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,12 +1,21 @@
 var frameModule = require("ui/frame");
+var observableModule = require("data/observable");
 
 var page;
 
+var loginCredentials = new observableModule.fromObject({
+    email: "",
+    password: ""
+});
+
 exports.loaded = function(args){
     page = args.object;
+    page.bindingContext = loginCredentials;
 };
 
 exports.signIn = function() {
+	// TODO: Check username & password with API and get access token
+	console.log("Loggin in with email: " + loginCredentials.email + " and password: " + loginCredentials.password);
     frameModule.topmost().navigate("views/activities/activities");
 };
 
--- app/views/signup/signup.js
+++ app/views/signup/signup.js
@@ -1,12 +1,22 @@
 var frameModule = require("ui/frame");
+var observableModule = require("data/observable");
 
 var page;
 
+var signUpCredentials = new observableModule.fromObject({
+    username: "",
+    email: "",
+    password: "",
+    password_confirm: ""
+});
+
 exports.loaded = function(args){
     page = args.object;
+    page.bindingContext = signUpCredentials;
 };
 
 exports.signUp = function() {
 	// TODO: Check if user credentials already taken and get access token
+	console.log("Signing up with username: " + signUpCredentials.username + " and email: " + signUpCredentials.email + " and password: " + signUpCredentials.password);
     frameModule.topmost().navigate("views/activities/activities");
 };
