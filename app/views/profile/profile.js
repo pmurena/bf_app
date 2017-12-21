6f731bf78057e44a30409d132bff04144604ff2e
Patrick Murena <patrick.murena@gmail.com>
2017-12-21 12:59:47 +0100

    added profile page

---
+++ app/views/profile/profile.js
@@ -0,0 +1,27 @@
+var frameModule = require("ui/frame");
+var Profile = require("../../shared/view-models/profile-view-model");
+var observableModule = require("data/observable")
+
+var profile = new Profile([]);
+var pageData = new observableModule.fromObject({
+    profile,
+    setEditable: false,
+    editSwitch: "editOff",
+    myProfile: profile.username == "tomasz" ? true: false
+});
+
+exports.loaded = function(args){
+  page = args.object;
+  page.bindingContext = pageData;
+  //profile.load();
+};
+
+exports.edit = function(){
+  if(pageData.setEditable){
+    page.bindingContext.set("editSwitch", "editOff");
+    page.bindingContext.set('setEditable', false);
+  }else{
+    page.bindingContext.set("setEditable", true);
+    page.bindingContext.set("editSwitch", "editOn");
+  }
+}
