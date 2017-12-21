3a89f6eccbfab07650a7401b1b362b4dd2c95a53
Patrick Murena <patrick.murena@gmail.com>
2017-12-22 00:33:51 +0100

    added read from and write to db for the profile page

--- app/shared/view-models/profile-view-model.js
+++ app/shared/view-models/profile-view-model.js
@@ -1,46 +1,87 @@
 var observableModule = require("data/observable");
-var ObservableArray = require("data/observable-array").ObservableArray;
 var fetchModule = require("fetch");
 var config = require("../config");
+var appSettings = require("application-settings");
 
-function Profile(items){
-  var viewModel = new observableModule.fromObject({
-    username: "tomasz",
-    firstname: "Thomas",
-    lastname: "Kolonko",
-    description: "Bacon ipsum dolor amet brisket beef sausage pastrami swine. Bresaola swine cupim boudin ham hock hamburger ham pork chop. Tongue burgdoggen turkey beef ribs andouille pig bresaola pork jowl pancetta tail pork chop capicola. Salami drumstick corned beef venison strip steak biltong.",
-    canton: "Bern",
-    country: "Switzerland",
-    email: "asdf@asdf.com",
-    website: "yolo.com",
-    phone: "078 788 88 88"
-  })
-//  viewModel.load = function(){
-//        console.log(config.apiUrl + "user/" + name)
-//        return fetch(config.apiUrl + "user/" + name)
-//            .then(function(response){
-//                return response.json();
-//            })
-//            .then(function(data){
-//                console.log("data")
-//                data.forEach(function(user){
-//                  console.log(usr)
-//                  viewModel.push({
-//                    name: activity.name,
-//                    description: activity.description
-//                  });
-//                });
-//            })
-//            .catch(function(error) {
-//              console.log(error);
-//            });
-//  };
+function Profile(id){
+  if(id === undefined){
+    id = appSettings.getString("id")
+  }
+  var viewModel = observableModule.fromObject({
+    id: id || "",
+    username: "",
+    firstname: "",
+    lastname: "",
+    description: "",
+    canton: "",
+    country: "",
+    email: "",
+    website: "",
+    phone: ""
+  });
+
+  viewModel.load = function(){
+        return fetch(config.apiUrl + "myProfile", {
+          method: "POST",
+      		body: JSON.stringify({
+      			"userToken": appSettings.getString("token")
+      		}),
+      		headers: {
+      			"Content-Type": "application/json",
+            'Authorization': 'Bearer ' + appSettings.getString("token")
+      		}
+      	})
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+          viewModel.id = data._id || ""
+          viewModel.username = data.username || "";
+          viewModel.firstname = data.firstname || "";
+          viewModel.lastname = data.lastname || "";
+          viewModel.description = data.description || "";
+          viewModel.canton = data.canton || "";
+          viewModel.country = data.country || "";
+          viewModel.email = data.email || "";
+          viewModel.website = data.website || "";
+          viewModel.phone = data.phon || "";
+          console.log(viewModel.id)
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+  };
 
   viewModel.empty = function() {
     while (viewModel.length) {
       viewModel.pop();
     }
   };
+
+
+  viewModel.save = function(){
+    fetch(config.apiUrl + "myProfile/edit", {
+      method: "POST",
+      body: JSON.stringify({
+        "token": appSettings.getString("token") || "",
+        "_id": appSettings.getString("id") || "",
+        "username": viewModel.username || "",
+        "firstname": viewModel.firstname || "",
+        "lastname": viewModel.lastname || "",
+        "description": viewModel.description || "",
+        "canton": viewModel.canton || "",
+        "country": viewModel.country || "",
+        "email": viewModel.email || "",
+        "website": viewModel.website || "",
+        "phone": viewModel.phone || ""
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer ' + appSettings.getString("token")
+      }
+    })
+  };
+
   return viewModel;
 }
 
--- app/views/profile/profile.js
+++ app/views/profile/profile.js
@@ -1,25 +1,27 @@
 var frameModule = require("ui/frame");
 var Profile = require("../../shared/view-models/profile-view-model");
 var observableModule = require("data/observable")
+var appSettings = require("application-settings");
 
-var profile = new Profile([]);
+var profile = new Profile();
 var pageData = new observableModule.fromObject({
     profile,
     setEditable: false,
     editSwitch: "editOff",
-    myProfile: profile.username == "tomasz" ? true: false
+    myProfile: profile.id == appSettings.getString("id") ? true: false
 });
 
 exports.loaded = function(args){
   page = args.object;
   page.bindingContext = pageData;
-  //profile.load();
+  profile.load();
 };
 
 exports.edit = function(){
   if(pageData.setEditable){
     page.bindingContext.set("editSwitch", "editOff");
     page.bindingContext.set('setEditable', false);
+    profile.save()
   }else{
     page.bindingContext.set("setEditable", true);
     page.bindingContext.set("editSwitch", "editOn");
